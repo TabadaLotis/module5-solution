d24896b9444597d1da1cb41c4c3b67ff921d6d76
TabadaLotis <tistabada@gmail.com>
2026-04-10 02:37:08 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,8 +1,12 @@
 (function (global) {
   const data = {};
 
-  data.categoriesUrl = "https://fake-restaurant-api.herokuapp.com/categories";
-  data.menuItemsUrl = "https://fake-restaurant-api.herokuapp.com/menu-items?category=";
+  const corsProxy = "https://cors-anywhere.herokuapp.com/";
 
+  // Original API URLs (keeping them as is)
+  data.categoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+  data.menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+
+  // Prepending the CORS Proxy to the API URLs
   global.$data = data;
 })(window);
