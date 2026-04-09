6d86444503b6560b606632dfe0632848c5a9dfbd
TabadaLotis <tistabada@gmail.com>
2026-04-10 01:55:08 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 (function (global) {
   const data = {};
 
+  // Use this working API for categories and menu items
   data.categoriesUrl =
     "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
   data.menuItemsUrl =
